7c59754672f729817f33be42931a5b24d974bbba
Faheem Ahmad <fisforfaheem@gmail.com>
2024-10-24 00:20:14 +0500

    udpates

--- lib/privacy.dart
+++ lib/privacy.dart
@@ -1,26 +1,2 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-
-//A privacy policy page that is displayed in a webview
-class WebViewPage extends StatelessWidget {
-  final String title;
-  final String url;
-
-  const WebViewPage({super.key, required this.title, required this.url});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: WebViewWidget(
-          controller: WebViewController()
-            ..loadRequest(Uri.parse(url))
-            ..setJavaScriptMode(JavaScriptMode.unrestricted),
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/quotes_list.dart
@@ -0,0 +1,160 @@
+import 'package:quotes_for_all/main.dart';
+
+final List<Quote> quotes = [
+  Quote(
+      text: "Be yourself; everyone else is already taken.",
+      author: "Oscar Wilde"),
+  Quote(
+      text:
+          "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+      author: "Albert Einstein"),
+  Quote(
+      text: "Be the change that you wish to see in the world.",
+      author: "Mahatma Gandhi"),
+  Quote(
+      text: "I have not failed. I've just found 10,000 ways that won't work.",
+      author: "Thomas Edison"),
+  Quote(
+      text: "If you tell the truth, you don't have to remember anything.",
+      author: "Mark Twain"),
+  Quote(
+      text: "A friend is someone who knows all about you and still loves you.",
+      author: "Elbert Hubbard"),
+  Quote(
+      text:
+          "I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character.",
+      author: "Martin Luther King Jr."),
+  Quote(
+      text: "The only way to do great work is to love what you do.",
+      author: "Steve Jobs"),
+  Quote(
+      text: "The only thing we have to fear is fear itself.",
+      author: "Franklin D. Roosevelt"),
+  Quote(
+      text: "The best way to predict the future is to create it.",
+      author: "Peter Drucker"),
+  Quote(
+      text:
+          "Success is not the absence of failure; it's the persistence through failure.",
+      author: "Aisha Tyler"),
+  Quote(
+      text:
+          "Success is liking yourself, liking what you do, and liking how you do it.",
+      author: "Maya Angelou"),
+  Quote(
+      text:
+          "The road to success and the road to failure are almost exactly the same.",
+      author: "Colin R. Davis"),
+  Quote(
+      text:
+          "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+      author: "Albert Schweitzer"),
+  Quote(
+      text:
+          "Success is not how high you have climbed, but how you make a positive difference to the world.",
+      author: "Roy T. Bennett"),
+  Quote(
+      text: "Your time is limited, don't waste it living someone else's life.",
+      author: "Steve Jobs"),
+  Quote(
+      text:
+          "The only way to achieve the impossible is to believe it is possible.",
+      author: "Charles Kingsleigh"),
+  Quote(
+      text:
+          "Success is not the absence of failure; it's the persistence through failure.",
+      author: "Aisha Tyler"),
+  Quote(
+      text:
+          "Success is liking yourself, liking what you do, and liking how you do it.",
+      author: "Maya Angelou"),
+  Quote(
+      text: "To live a creative life, we must lose our fear of being wrong.",
+      author: "Anonymous"),
+  Quote(
+      text:
+          "If you are not willing to risk the usual, you will have to settle for the ordinary.",
+      author: "Jim Rohn"),
+  Quote(
+      text:
+          "Trust because you are willing to accept the risk, not because it's safe or certain.",
+      author: "Anonymous"),
+  Quote(
+      text:
+          "All our dreams can come true if we have the courage to pursue them.",
+      author: "Walt Disney"),
+  Quote(
+      text:
+          "Good things come to people who wait, but better things come to those who go out and get them.",
+      author: "Anonymous"),
+  Quote(
+      text: "If you do what you always did, you will get what you always got.",
+      author: "Anonymous"),
+  Quote(
+      text:
+          "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+      author: "Albert Schweitzer"),
+  Quote(
+      text:
+          "Success is not how high you have climbed, but how you make a positive difference to the world.",
+      author: "Roy T. Bennett"),
+  Quote(
+      text: "Your time is limited, don't waste it living someone else's life.",
+      author: "Steve Jobs"),
+  Quote(
+      text: "The only way to do great work is to love what you do.",
+      author: "Steve Jobs"),
+  Quote(
+      text: "The only thing we have to fear is fear itself.",
+      author: "Franklin D. Roosevelt"),
+  Quote(
+      text: "The best way to predict the future is to create it.",
+      author: "Peter Drucker"),
+  Quote(
+      text:
+          "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+      author: "Winston Churchill"),
+  Quote(
+      text: "Don't watch the clock; do what it does. Keep going.",
+      author: "Sam Levenson"),
+  Quote(
+      text:
+          "Keep your face always toward the sunshine—and shadows will fall behind you.",
+      author: "Walt Whitman"),
+  Quote(
+      text:
+          "The only limit to our realization of tomorrow is our doubts of today.",
+      author: "Franklin D. Roosevelt"),
+  Quote(
+      text:
+          "The future belongs to those who believe in the beauty of their dreams.",
+      author: "Eleanor Roosevelt"),
+  Quote(
+      text: "It does not matter how slowly you go as long as you do not stop.",
+      author: "Confucius"),
+  Quote(
+      text: "Everything you’ve ever wanted is on the other side of fear.",
+      author: "George Addair"),
+  Quote(
+      text: "Believe you can and you're halfway there.",
+      author: "Theodore Roosevelt"),
+  Quote(
+      text: "Act as if what you do makes a difference. It does.",
+      author: "William James"),
+  Quote(
+      text:
+          "Success usually comes to those who are too busy to be looking for it.",
+      author: "Henry David Thoreau"),
+  Quote(
+      text: "Don't be afraid to give up the good to go for the great.",
+      author: "John D. Rockefeller"),
+  Quote(
+      text: "I find that the harder I work, the more luck I seem to have.",
+      author: "Thomas Jefferson"),
+  Quote(
+      text: "Success is not in what you have, but who you are.",
+      author: "Bo Bennett"),
+  Quote(
+      text: "The way to get started is to quit talking and begin doing.",
+      author: "Walt Disney"),
+];
